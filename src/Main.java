ce24eb4c454732fe96d9a35d0daf808afcdf4e50
Oleksandr Zhurba <starkyzh@gmail.com>
2023-05-23 17:23:59 +0200

    style

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,30 @@
 import java.text.ParseException;
-import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        String name1 = "name1";
-        String name2 = "name2";
-
 
         List<Person> person = new ArrayList<>();
+
+        String name1 = "name1";
         String birthDate1 = "27/11/1983";
-        String birthDate2 = "24/06/1984";
-        Person person1 = new Person(name1,birthDate1);
+        Person person1 = new Person(name1, birthDate1);
         person.add(person1);
-        Person person2 = new Person(name2,birthDate2);
+
+        String name2 = "name2";
+        String birthDate2 = "24/06/1984";
+        Person person2 = new Person(name2, birthDate2);
         person.add(person2);
-        for (Person p: person
-             ) {
-            System.out.println(p.getName()+"___"+p.getBirthDate());
+
+        for (Person p : person
+        ) {
+            System.out.println(p.getName() + "___" + p.getBirthDate());
         }
         System.out.println("======================");
         Collections.sort(person);
-        for (Person p: person
+        for (Person p : person
         ) {
-            System.out.println(p.getName()+"___"+p.getBirthDate());
+            System.out.println(p.getName() + "___" + p.getBirthDate());
         }
     }
 }
